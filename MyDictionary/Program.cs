7e9bf85c05d3f9f9abdc4411a745ea6ccd1df6db
haydar özgür mısırlı <haydaremre31@gmail.com>
2021-01-23 20:14:35 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> games = new Dictionary<int, string>();
+            games.Add(1,"Forza Horizon 4");
+            games.Add(2,"Battlefield 1");
+            games.Add(3,"Grand Theft Auto");
+
+            foreach (KeyValuePair<int,string> game  in games)
+            {
+                Console.WriteLine("İf you choose this {0} result : {1}",game.Key,game.Value);
+            }
+        }
+    }
+}
